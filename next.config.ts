bfd5edcbde0026c6396a8495f82b9adc140b56f4
juye-ops <kjye.ops@gmail.com>
2026-04-11 20:06:56 +0900

    🎉 init: add deploying workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // 정적 HTML로 내보내기 설정
+  images: {
+    unoptimized: true, // GitHub Pages는 이미지 최적화 서버를 지원하지 않음
+  },
 };
 
 export default nextConfig;
